fe97b84cbe3f313d057f6d1fdbfc89c456975b75
Alle4n <allennajjuma96@gmail.com>
2025-02-23 22:36:03 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,7 +35,11 @@ function App() {
       <div className="App">
             <WelcomeMessage />
       </div>
-
+      <div className="App">
+            <Header />
+            <MainContent />
+            <Footer />
+      </div>
     </>
     
   )
