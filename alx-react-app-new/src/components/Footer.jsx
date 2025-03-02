1ee0443ad24a6afd52c5cccf7a7684cab95369cc
Alle4n <allennajjuma96@gmail.com>
2025-03-02 21:53:28 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'darkslategray', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
